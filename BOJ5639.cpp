41e79471a5df863866b0abd30cd86f6c2fba1bfc
Yerim Jung <yerimy97@gmail.com>
2019-11-11 20:04:56 +0900

    BOJ 5639/solved

--- BOJ5639.cpp
+++ BOJ5639.cpp
@@ -8,23 +8,45 @@ struct Node {
 	Node* ch2 = NULL;
 	int n = 0;
 };
+vector<Node*> v;
 Node* root = new Node;
-void pre(vector<int>v) {
-	int tsize = v.size();
-	root->n = v[0];
-	for (int i = 1; i < tsize; i++) {
-		
+void pre(int x,Node* N) {
+	if (x < N->n)
+	{
+		if (N->ch1 == NULL) {
+			Node* newN = new Node;
+			newN->n = x;
+			newN->pa = N;
+			N->ch1 = newN;
+			v.push_back(newN);
+		}
+		else pre(x, N->ch1);
+	}
+	else {
+		if (N->ch2 == NULL) {
+			Node* newN = new Node;
+			newN->n = x;
+			newN->pa = N;
+			N->ch2 = newN;
+			v.push_back(newN);
+		}
+		else pre(x, N->ch2);
 	}
 }
-void post() {
-
+void post(Node* N) {
+	if (N->ch1 != NULL)
+		post(N->ch1);
+	if (N->ch2 != NULL)
+		post(N->ch2);
+	cout << N->n << "\n";
 }
-int main(){
+int main() {
 	int tmp = 0;
-	vector<int> v;
-	while (cin >> tmp) 
-		v.push_back(tmp);
-	pre(v);
-	post();
+	cin >> tmp;
+	root->n = tmp;
+	v.push_back(root);
+	while (cin >> tmp)
+		pre(tmp,root);
+	post(root);
 	return 0;
 }
