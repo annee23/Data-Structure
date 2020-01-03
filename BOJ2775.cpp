733a52faa5546c97749b05bde3637cd1562887a1
Yerim Jung <yerimy97@gmail.com>
2020-01-03 18:52:18 +0900

    BOJ 2775

--- BOJ2775.cpp
+++ BOJ2775.cpp
@@ -4,22 +4,22 @@ using namespace std;
 int main() {
 	int t, k, n;
 	cin >> t;
+
 	int arr[15][15];
-	for (int i = 1; i < 15; i++)
+	for (int i = 1; i < 15; i++)	//0층,1호들 초기화
 	{
 		arr[0][i] = i;
+		arr[i][1] = 1;
 	}//arr[a][b]=arr[a][b-1]+arr[a-1][b]/ arr[a][1]=arr[a-1][1]
 	for (int i = 1; i < 15; i++) {
-		arr[i][1] = arr[i - 1][1];
 		for (int j = 2; j < 15; j++) {
-			arr[i][j] = arr[i - 1][j];
+			arr[i][j] = arr[i - 1][j]+arr[i][j-1];
 		}
 	}
 	
 	while (t--) {
 		cin >> k >> n;
-		while (k--) {
-
-		}
+		cout << arr[k][n] << endl;
 	}
+	return 0;
 }
